713f44d8fc6cf4a2d4518ec33615bcfcae6addaf
Anders Ödlund <odlund@gmail.com>
2015-10-24 14:32:05 -0700

    Initial Commit

---
+++ MyAwesomeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyAwesomeApp
+//
+//  Created by Anders Oedlund on 10/24/15.
+//  Copyright © 2015 Anders Oedlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyAwesomeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAwesomeApp
+//
+//  Created by Anders Oedlund on 10/24/15.
+//  Copyright © 2015 Anders Oedlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
