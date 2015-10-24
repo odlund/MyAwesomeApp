d9c92f273a2e3a87fcf190e09b95302bf3931da6
Anders Ödlund <odlund@gmail.com>
2015-10-24 16:47:47 -0700

    Added ReactiveCocoa

--- MyAwesomeApp/ViewController.m
+++ MyAwesomeApp/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    [RACSignal startEagerlyWithScheduler:[RACScheduler immediateScheduler] block:^(id<RACSubscriber> subscriber) {
+        NSLog(@"Hello World");
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
